5df8f7c0e327994e94f320c86d35bf5cb6fc30f7
Isaac Kim <isaackim@Isaacs-MacBook-Air.local>
2025-01-31 14:08:02 -0500

    completed for loop and break section of lesson

--- lesson-4/js/loops.js
+++ lesson-4/js/loops.js
@@ -1,9 +1,23 @@
 let courses = ['Intro to Web Programming', 'Client-Side JavaScript', 'Bitmap & Vector Graphics', 'Web UI Design with CSS', 'Relational Database'];
-let courseList = 'My courses this term are ';
+let courseList = 'My courses this term are: ';
 const msg = document.querySelector('#courses');
 /* STEP 1a: A simple FOR loop
 A simple loop of this structure needs an initializer (counter variable), an exit conditional, and and incrementor/decrementor */
-
+for (let i = 0; i < courses.length; i ++){
+  //add each element with that
+    //courseList = courseList + " " + courses[i];
+  //do increment calculator instead
+    //courseList += " " + courses[i];
+  //check to see if we are on the last course in the list
+  if(i === courses.length -1){
+    //finish the sentence properly
+    courseList += `and ${courses[i]}.`;
+  } else{
+    // add each course to the sentence
+    //or do string interpolation
+    courseList += `${courses[i]}, `;
+  }
+};
 msg.textContent = courseList;
 // STEP 1b: Add a conditional for the last item in the array so we can finish the sentence with a period.
 
@@ -14,15 +28,25 @@ const input = document.querySelector('input');
 const btn = document.querySelector('button#cities');
 
 btn.addEventListener('click', function() {
-	var searchName = input.value;
+	let searchName = input.value;
+  console.log(searchName);
 	input.value = '';
+  //we returned focus back to the search
 	input.focus();
 	/* STEP 2a: Create a FOR loop to iterate through the cities array */
-
+  for (let i = 0; i < cities.length; i ++){
+    //console.log(cities[i]);
 		/* STEP 2b: Build an IF/ELSE conditional that compares searchName with cities[i] */
-
+    if (searchName === cities[i]){
+      // Message that the city in the search was in the array
+      result.textContent = `${searchName} is in the citties array. `;
+      //we found the city so stop looking and exit the loop
+      break;
+    } else {
+      result.textContent = `${searchName} is NOT the citties array. `;
+    };
+  };
 });
-
 
 
 /* STEP 3: Skip a loop interation with continue */
