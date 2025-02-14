b4a1f41270a09deeb21f202baff7dff76ce159c0
Isaac Kim <isaackim@Isaacs-MacBook-Air.local>
2025-02-14 15:12:31 -0500

    completed the gallery, only the the event delgation remaining

--- lesson-6/js/gallery.js
+++ lesson-6/js/gallery.js
@@ -1,16 +1,27 @@
 // STEP 1: Initialize and declare variables
-
+const displayedImage = document.querySelector('.displayed-img');
+const thumbBar = document.querySelector('.thumb-bar');
 
 /* STEP 2: Loop 5 times to create the <img> elements */
-
+for (let i = 1; i <= 5; i++) {
 		/* STEP 3a: Create a new DOM node - an image element */
-
+    const newImage = document.createElement('img');
 		/* STEP 3b: Set the src attribute to be the path of one of the images inside the images folder, using setAttribute() */
-
-		/* Append the new image element to the thumbBar div, named in STEP 1 */
-
+    newImage.setAttribute('src', `images/pic${i}.jpg`);
+		//good practice to make sure it works and loading the right images
+    console.log(newImage);
+    /* Append the new image element to the thumbBar div, named in STEP 1 */
+    //we are appending the new image element inside the thumbBar div
+    thumbBar.append(newImage);
 		/* STEP 3c: Build event handler for each <img> */
-
+  newImage.addEventListener('click', function(event) {
+    console.log(event.target);
+    let imgSrc = event.target.getAttribute('src'); //grab the src attribute of the element that was clicked
+    //console.log(imgSrc);
+    //set main image to the src of the clicked image
+    displayedImage.setAttribute('src', imgSrc);
+  });
+};
 
 /* STEP 4: Function to change the src of the main <img> */
 
