7348bdbb43ca0d3cae079b48f181d5d59a8f3382
Isaac Kim <isaackim@Isaacs-MacBook-Air.local>
2025-03-14 14:16:22 -0400

    finished the object prototype interitance script

--- lesson-9/js/index.js
+++ lesson-9/js/index.js
@@ -1,9 +1,20 @@
 const output = document.querySelector('#output');
 
 /* STEP 1: Create an object (looks a lot like declaring a variable, but with empty braces), then open this page in a browser and enter 'Coffee' in the console */
-
+function Coffee(size, isDecaf, qtyCream, qtySugar){
+  this.size = size;
+  //this.isDecaf = isDecaf;  // these are properties
+  //create a condition instead writing true or false for "isdecaf"
+  // using ternary, if this variable is true do this or else (:) do this if false
+  this.decaf = (this.isDecaf == true ? "decafeinated" : "caffeinated");
+  this.qtyCream = qtyCream;
+  this.qtySugar = qtySugar;
+  this.description = function(){
+    output.textContent = `A ${this.size} ${isDecaf} coffee with ${this.qtyCream} creamers and ${this.qtySugar} teaspoons of sugar`;
+  }
+}
 /* STEP 2: Instatiate a coffee based on the above constructor function */
-
+let isaacsCoffee = new Coffee("large", false, 2, 1) //Instatiate a new coffee object using the new keyword
 /* STEP 3: Refresh the page, and in the console, begin to call a method on scottsCoffee by typing 'scottsCoffee.' - look at all the members and methods */
 
 /* STEP 4: Enter into the console, scottsCoffee.valueOf() and look at the result. scottsCoffee doesn't have such a method, and neither does the constructor function, 'Coffee'. But the 'Object' object does - so through inheritance, scottsCoffee has access to the method, valueOf(). */
@@ -15,13 +26,19 @@ const output = document.querySelector('#output');
 /* STEP 5c: EVERYTHING is an object in JavaScript. Try accessing the prototype property of Coffee (which even though it is a constructor function it is still an object) with Coffee.prototype in the console. Then try Object.Prototype */
 
 /* STEP 6a: Let's circle back to create() - use scottsCoffee to create a new object instance - one based on scottsCoffee. */
-
+let megansCoffee = Object.create(isaacsCoffee);
+//we can alter from 2 to 1
+megansCoffee.qtyCream = 1;
+//alter from false to true
+megansCoffee.isDecaf = true;
 /* STEP 6b: See how this new object inherits from the prototype with richsCoffee.__proto__ in the console. */
-
+//megansCoffee.__proto__ in the console
+//megansCoffee.__proto__.__proto__ in the console
 /* STEP 7a: Each constructor function includes a prototype property with a value equal to an object that contains a constructor property. Try it out by typing scottsCoffee.constructor and richsCoffee.constructor */
 
 /* STEP 7b: Since constructor is also a function, you can use it to create a new object instance - try it! */
-
+//might be helpful if you don't have direct access to the codebase
+let lukesCoffee = new megansCoffee.constructor("medium", true, 2, 2);
 /* STEP 7c: Attempt via the console to access the new object's properties - kathysCoffee.size, kathysCoffee.isDecaf, etc. */
 
 /* STEP 7d: Now see if the new object can access the description() method… */
@@ -29,7 +46,12 @@ const output = document.querySelector('#output');
 /* STEP 7e: The constructor has other features - try using it to discover the name of an instance's constructor by typing kathysCoffee.constructor.name into the console. */
 
 /* STEP 8a: We can modify the prototype property of a constructor function - let's add another method to Coffee */
-
+//function name is "thanks"
+// dunno what prototype means though
+Coffee.prototype.thanks = function() {
+  output.textContent = `thanks for a ${this.size} coffee! `;
+} // added more abilities, they now have the abilities to thanks.
+// in the console do "lukesCoffee.thanks()", "isaacsCoffee.thanks()", "lukesCoffee.thanks()"
 // What's really interesting about the above code is that every instance based on the Coffee constructor has been updated due to prototypical inheritance!
 
 /* STEP 8b: Now let's call up this method on one or more of our Coffee instances from the console (scottsCoffee, richsCoffee, or kathysCoffee). */
